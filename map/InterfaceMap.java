9d8d28b1a28ed80cffd9abe724d4eed5385e439e
uniked <ra-22229736-5@alunos.unicesumar.edu.br>
2022-10-31 23:43:04 -0300

    Métodos Map

---
+++ map/InterfaceMap.java
@@ -0,0 +1,79 @@
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+/**
+ * Map
+ */
+public class InterfaceMap {
+
+    public static void main(String[] args) {
+    
+        /**
+         * Dado os modelos dos carros e seus respectivos consumos na estrada
+         */
+    System.out.println("Crie um dicionário que relacione os modelos respectivos");
+
+    Map<String, Double> carrosPopulares = new HashMap<>(){{
+        put("gol", 14.4);
+        put("uno", 15.6);
+        put("mobi", 16.1);
+        put("hb20", 14.5);
+        put("kwid", 15.6);
+    }};
+    System.out.println(carrosPopulares.toString());
+
+    System.out.println("Substitua o valor do gol por 15,2 km/l");
+    carrosPopulares.put("gol",15.2);
+    System.out.println(carrosPopulares);
+
+    System.out.println("Confira se o modelo tucson está no dicionário: " + carrosPopulares.containsKey("tucson"));
+
+    System.out.println("Exiba o consumo do uno: " + carrosPopulares.get("uno"));
+
+    System.out.println("Exiba os modelos: " + carrosPopulares.keySet());
+    Set<String> modelos = carrosPopulares.keySet(); //mostra o mesmo acima
+    System.out.println(modelos);
+
+    System.out.println("Exiba o comsumo dos carros: " + carrosPopulares.values());
+    Collection<Double> consumos = carrosPopulares.values(); //mesma representação que acima
+    System.out.println(consumos);
+
+    System.out.println("Exiba o modlo mais economico e seu consumo");
+
+    Double consumoMaisEficiente =  Collections.max(carrosPopulares.values());
+    Set<Map.Entry<String,Double>>entries = carrosPopulares.entrySet();
+    String modeloMaisEficiente = "";
+
+
+for (Map.Entry<String,Double> entry:entries){
+    if (entry.getValue().equals(consumoMaisEficiente))modeloMaisEficiente = entry.getKey();
+    
+    System.out.println("Modelo mais eficiente: " + modeloMaisEficiente + " - " + consumoMaisEficiente);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
