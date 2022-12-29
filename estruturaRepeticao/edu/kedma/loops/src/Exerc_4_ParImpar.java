f5368521679ca8c3922b0e2e19365f8672ca8db0
kedinha <kedmyknow@gmail.com>
2022-12-29 10:20:31 -0300

    Exercicio Par Impar

---
+++ estruturaRepeticao/edu/kedma/loops/src/Exerc_4_ParImpar.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+/*
+Faça um programa que peça N números inteiros,
+calcule e mostre a quantidade de números pares
+e a quantidade de números impares.
+*/
+
+public class Exerc_4_ParImpar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int nNumeros;
+        int numero;
+        int quantPar = 0, quantImpar = 0;
+
+        System.out.print("Quantidade de números: ");
+
+        nNumeros = scan.nextInt();
+
+        int count = 0;
+
+        do {
+            System.out.print("Número: ");
+
+            numero = scan.nextInt();
+
+            if (numero % 2 == 0)
+                quantPar++;
+            else
+                quantImpar++;
+
+            count++;
+
+        } while (count < nNumeros);
+
+        System.out.println("Quantidade de Números Pares: " + quantPar);
+        System.out.println("Quantidade de Núemros Impares: " + quantImpar);
+
+        scan.close();
+    }
+
+}
