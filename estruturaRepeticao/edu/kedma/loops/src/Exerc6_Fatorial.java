e9fc1a138e977b103627cfc70cc9a429057f91fe
kedinha <kedmyknow@gmail.com>
2022-12-29 16:08:20 -0300

    Fatorial

---
+++ estruturaRepeticao/edu/kedma/loops/src/Exerc6_Fatorial.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/*
+Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
+Ex.: 5!= 120 (5 X 4 X 3 X 2 X 1)
+*/
+
+public class Exerc6_Fatorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+
+        // System.out.print("Fatorial de: ");
+        int fatorial = scan.nextInt();
+
+        int multiplicacao = 1;
+
+        System.out.println("Fatorial de: "+ fatorial + "!");
+        for (int i = fatorial; i >= 1; i--) {
+
+            multiplicacao = multiplicacao * i;
+            System.out.println(multiplicacao + " X " + i + " = "+ multiplicacao);
+        }
+        // System.out.println(multiplicacao);
+
+        scan.close();
+    }
+}
